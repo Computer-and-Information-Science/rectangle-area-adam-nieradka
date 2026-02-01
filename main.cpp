4bc385a9f597cf0fbd2fceac56527f7d6325ee05
adam-nieradka <adam.nieradka@student.northampton.edu>
2026-02-01 18:51:15 +0000

    Wrote program to calculate area and perimeter and to display rectangle properties.

--- main.cpp
+++ main.cpp
@@ -5,5 +5,16 @@ int main() {
   const double length = 7.2;
   const double width = 1.3;
 
+  double area = length * width;
+  int perimeter = 2 * (length + width);
+
+  cout << "Rectangle Properties: " << endl;
+  cout << "Length = " << length << endl;
+  cout << "Width = " << width << endl;
+  cout << "Area = " << area << endl;
+  cout << "Perimeter = " << perimeter << endl;
+
+  return 0;
+
 
 }
